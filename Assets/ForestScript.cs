0fafb99429f82b1045e84fb170401369c8f8660a
Millie <millie@katproductions.co.uk>
2025-03-19 13:19:31 +1300

    finalised prototype

--- Assets/ForestScript.cs
+++ Assets/ForestScript.cs
@@ -4,15 +4,15 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 
-public class DialogueTest : MonoBehaviour
+public class ForestScript : MonoBehaviour
 {
-    public static DialogueTest current;
+    public static ForestScript current;
 
-    [SerializeField] private GameObject prompt;
     [SerializeField] private GameObject dialogueVisuals;
     //[SerializeField] private TextMeshProUGUI dialogueName;
     [SerializeField] private TextMeshProUGUI dialogueTextbox;
     [SerializeField] private Image characterImage;
+    [SerializeField] private Sprite sprite;
 
     //[TextArea] public string characterName;
     [TextArea] public string dialogue;
@@ -34,24 +34,19 @@ public class DialogueTest : MonoBehaviour
 
     private void Update()
     {
-        if (playerFound && Input.GetKeyDown(KeyCode.E))
+        if (playerFound)
         {
             StartCoroutine(DialogueDelay());
             playerMovement.SetMovement(false);
             dialogueVisuals.SetActive(true);
-            prompt.SetActive(false);
             //dialogueName.text = characterName;
+            characterImage.sprite = sprite;
             dialogueTextbox.text = dialogue;
-            characterImage.sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
             isInteracting = true;
         }
 
         if (isInteracting)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene("MinigameSolitaire");
-            }
             if (dialogueFinished && Input.GetKeyDown(KeyCode.E))
             {
                 EndDialogue();
@@ -62,7 +57,6 @@ public class DialogueTest : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         playerFound = true;
-        prompt.SetActive(true);
     }
 
     public void OnTriggerExit(Collider other)
@@ -73,7 +67,6 @@ public class DialogueTest : MonoBehaviour
     public void EndDialogue()
     {
         playerFound = false;
-        prompt.SetActive(false);
         dialogueVisuals.SetActive(false);
         isInteracting = false;
         playerMovement.SetMovement(true);
