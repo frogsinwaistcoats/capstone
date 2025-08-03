9b6e45c672570aa595590ba17da4da215d627652
frogsinwaistcoats <millie@katproductions.co.uk>
2025-08-03 16:20:02 +1200

    go back to here if event system fails

--- Assets/Scripts/QuestSystem/QuestManager.cs
+++ Assets/Scripts/QuestSystem/QuestManager.cs
@@ -10,6 +10,38 @@ public class QuestManager : MonoBehaviour
         questMap = CreateQuestMap();
     }
 
+    private void OnEnable()
+    {
+        GameEventsManager.instance.questEvents.onStartQuest += StartQuest;
+        GameEventsManager.instance.questEvents.onAdvanceQuest += AdvanceQuest;
+        GameEventsManager.instance.questEvents.onFinishQuest += FinishQuest;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.questEvents.onStartQuest -= StartQuest;
+        GameEventsManager.instance.questEvents.onAdvanceQuest -= AdvanceQuest;
+        GameEventsManager.instance.questEvents.onFinishQuest -= FinishQuest;
+    }
+
+    private void StartQuest(string id)
+    {
+        // TODO - start the quest
+        Debug.Log("Start quest: " + id);
+    }
+
+    private void AdvanceQuest(string id)
+    {
+        // TODO - advance the quest
+        Debug.Log("Advance quest: " + id);
+    }
+
+    private void FinishQuest(string id)
+    {
+        // TODO - finish the quest
+        Debug.Log("Finish quest: " + id);
+    }
+
     private Dictionary<string, Quest> CreateQuestMap()
     {
         // loads all QuestInfoSO scriptable objects under the assets/resources/quests folder
@@ -27,5 +59,13 @@ public class QuestManager : MonoBehaviour
         return idToQuestMap;
     }
 
-
+    private Quest GetQuestById(string id)
+    {
+        Quest quest = questMap[id];
+        if (quest == null)
+        {
+            Debug.LogError("ID not found in the quest map: " + id);
+        }
+        return quest;
+    }
 }
