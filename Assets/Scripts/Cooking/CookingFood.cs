ac686fc3cfd20c2391c2bd491449fd237a7a4990
frogsinwaistcoats <millie@katproductions.co.uk>
2025-09-19 16:02:41 +1200

    more cooking stuff
    mostly there i think, sometimes food gets stuck and breaks whole game :(

--- Assets/Scripts/Cooking/CookingFood.cs
+++ Assets/Scripts/Cooking/CookingFood.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 
 public class CookingFood : MonoBehaviour
 {
-    public bool isBeingHeld;
+    
     public GameObject tile;
+    public Transform[] raycastTransforms;
+    //public Collider2D objCollider;
+    public SpriteRenderer sr;
+
+    public Vector2 pivotOffset;
+    public Transform[] gridTargets;
+
+    private Vector3 startPos;
+
     public LayerMask boardLayer;
     public LayerMask foodLayer;
-    public Transform[] raycastTransforms;
+
+    public bool isBeingHeld;
+    public bool isOnBoard;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        //objCollider = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
@@ -30,73 +48,87 @@ public class CookingFood : MonoBehaviour
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPosition.z = 0f;
         transform.position = mouseWorldPosition;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector3(0f, 0f, 10f), Mathf.Infinity, foodLayer);
-        if (hit.collider != null)
-        {
-            Debug.Log(gameObject.name + " is over " + hit.collider.gameObject.name);
-        }
     }
 
 
     private void OnMouseDown()
     {       
         isBeingHeld = true;
-        //tile.SetActive(true);    
+        isOnBoard = false;
+        sr.sortingOrder = 10; // Bring to front while dragging
+        tile.GetComponent<SpriteRenderer>().sortingOrder = 9; // Ensure tile is just behind the food
+
     }
 
     private void OnMouseUp()
     {
         bool isBlocked = false;
 
-        //checking for food underneath
         foreach (Transform raycastTransform in raycastTransforms)
         {
-            RaycastHit2D hit = Physics2D.Raycast(raycastTransform.position, new Vector3(0f, 0f, 10f), Mathf.Infinity, foodLayer);
-            if (hit.collider != null)
+            // Check for overlap with food items
+            Collider2D hitFood = Physics2D.OverlapPoint(raycastTransform.position, foodLayer);
+            if (hitFood != null && hitFood.gameObject != gameObject)
             {
                 isBlocked = true;
+                Debug.Log(gameObject.name + " " + raycastTransform.name + " is over " + hitFood.name + ", cannot be placed");
+                break;
+            }
+
+            // Check for overlap with board
+            Collider2D hitBoard = Physics2D.OverlapPoint(raycastTransform.position, boardLayer);
+            if (hitBoard != null)
+            {
+                //Debug.Log(raycastTransform.name + " is over Board");
+            }
+            else
+            {
+                isBlocked = true;
+                Debug.Log(gameObject.name + " " + raycastTransform.name + " is not over Board, cannot be placed");
                 break;
             }
         }
 
+        // Stop holding the item
+        isBeingHeld = false;
+        sr.sortingOrder = 5; // Reset sorting order
+        tile.GetComponent<SpriteRenderer>().sortingOrder = 4; // Reset tile sorting order
+
+        // if blocked, return to start position, else snap to grid
         if (isBlocked)
         {
-            Debug.Log(gameObject.name + " is blocked by other food");
-            isBeingHeld = true;
-            //transform.position = hits[0].collider.transform.position;
-            //tile.SetActive(false);
-        }
-        if (!isBlocked)
-        {
-            isBeingHeld = false;
-        }
-
-
-        bool isOnBoard = false;
-        //checking for board underneath
-
-        foreach (Transform raycastTransform in raycastTransforms)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(raycastTransform.position, new Vector3(0f, 0f, 10f), Mathf.Infinity, boardLayer);
-            if (hit.collider == null)
-            {
-                isOnBoard = false;
-                break;
-            }
-            isOnBoard = true;
-
-        }
-
-        if (isOnBoard)
-        {
-            isBeingHeld = false;
-            //transform.position = hits[0].collider.transform.position;
-            //tile.SetActive(false);
+            transform.position = startPos;
         }
         else
         {
-            Debug.Log(gameObject.name + " is not over Board");
-            isBeingHeld = true;
+            isOnBoard = true;
+            SnapToGrid();
+        }
+    }
+
+    private void SnapToGrid()
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector2 pivotPosition = (Vector2)transform.position + pivotOffset;
+
+        foreach (Transform potentialTarget in gridTargets)
+        {
+            if (potentialTarget == null) continue;
+
+            Vector2 direction = (Vector2)potentialTarget.position - pivotPosition;
+            float distanceSqr = direction.sqrMagnitude;
+
+            if (distanceSqr < closestDistanceSqr)
+            {
+                closestDistanceSqr = distanceSqr;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        if (bestTarget != null)
+        {
+            transform.position = (Vector2)bestTarget.position - pivotOffset;
         }
 
     }
