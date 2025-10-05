3a7c0c9374f5c82639b2c39d2996c973b799d1d0
frogsinwaistcoats <millie@katproductions.co.uk>
2025-10-05 22:07:50 +1300

    more leaf game

--- Assets/Scripts/Leaf/LeafHolder.cs
+++ Assets/Scripts/Leaf/LeafHolder.cs
@@ -1,22 +1,35 @@
-using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LeafHolder : MonoBehaviour
 {
     public static LeafHolder instance;
 
-    public List<GameObject> leaves = new List<GameObject>();
+    public GameObject[] leafGroup;
+    public GameObject[] leafGroupOutlines;
 
-    public Transform slotOne;
-    public Transform slotTwo;
-    public Transform slotThree;
-    public Transform slotFour;
+    public List<GameObject> leaves = new List<GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        // ADD LATER to make sure the first group is selected for the first playthrough, then second group for the next playthrough, etc.
+
+        int selectedGroup = Random.Range(0, leafGroup.Length - 1);
+
+        leaves.AddRange(leafGroup[selectedGroup]
+            .GetComponentsInChildren<Transform>(true) // gets all transforms (parent + children)
+            .Where(t => t != leafGroup[selectedGroup].transform) // keep only children
+            .Select(t => t.gameObject) // get the game object for each child
+        );
+
+        leafGroupOutlines[selectedGroup].SetActive(true);
 
         ShuffleLeaves();
     }
@@ -35,11 +48,5 @@ public class LeafHolder : MonoBehaviour
     public void RemoveLeaf()
     {
         leaves.RemoveAt(0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
